6dc8a9102ec52b712ed5681a6a97b0cab53083ac
Kztka <paper.sheet.kami@gmail.com>
2016-11-04 08:00:22 +0900

    reply method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :homes
   post '/callback' => 'webhook#callback'
-  post '/fortune' => 'fortune'
+  post '/fortune' => 'fortune#callback'
   root :to => 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
