b150abcb21991dff4390cfbf9bbcca4f06f16c78
Kztka <paper.sheet.kami@gmail.com>
2016-11-04 09:29:10 +0900

    reply method

--- app/models/message/formatter/fortune_message.rb
+++ app/models/message/formatter/fortune_message.rb
@@ -2,7 +2,6 @@ require 'date'
 
 class FortuneMessage
   def output_message(context)
-    p "AAAAAAAA"
     p receive_api
     {
         type: "text",
@@ -11,13 +10,6 @@ class FortuneMessage
   end
 
   def receive_api()
-    today = Date.today
-    year = today.year.to_s
-    month = today.month.to_s
-    day = today.day.to_s
-
     HTTParty.get('http://api.jugemkey.jp/api/horoscope/free')
-
-
   end
 end
