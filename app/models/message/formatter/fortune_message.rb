c76cc5019c778c9e4523cf5f2ebbae0ad602d627
Kztka <paper.sheet.kami@gmail.com>
2016-11-04 09:08:27 +0900

    reply method

--- app/models/message/formatter/fortune_message.rb
+++ app/models/message/formatter/fortune_message.rb
@@ -10,12 +10,13 @@ class FortuneMessage
   end
 
   def receive_api()
-    year = Date.year.to_s
-    month = Date.month.to_s
-    today = Date.today.to_s
+    today = Date.today
+    year = today.year
+    month = today.month
+    day = today.day
 
     headers = {
-        "X-JUGEMKEY-API-CREATED": "2006-07-26T06:55:44Z",
+        'X-JUGEMKEY-API-CREATED': '2006-07-26T06:55:44Z',
         "X-JUGEMKEY-API-KEY": "f6fe8e8481ab91c4785eec03e93c505b",
         "X-JUGEMKEY-API-SIG": "8720df454dd79b9c7acfa3338b35a17273f54472"
     }
